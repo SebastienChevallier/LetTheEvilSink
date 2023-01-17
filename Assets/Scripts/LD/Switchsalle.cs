956fbe3f31743e86988807e84b04294abf4f619b
Sebastien chevallier <44536776+The-Real-Seb@users.noreply.github.com>
2023-01-17 16:14:03 +0100

    Prefabs et shader camera

--- Assets/Scripts/LD/Switchsalle.cs
+++ Assets/Scripts/LD/Switchsalle.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class Switchsalle : MonoBehaviour
 {
+    
     public float speedRotation;
     public float valRotaDeg;
     public float baseRotaDeg;
     public GameObject inverseTrigger;
     public GameObject inverseTrigger2;
+
+    public bool hasColliderCam;
+    public GameObject colliderCam;
+    public GameObject colliderCam2;
 
     public GameObject obj = null;
 
@@ -26,25 +31,41 @@ public class Switchsalle : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.E))
             {
+                /*if (hasColliderCam)
+                {
+                    colliderCam.SetActive(false);
+                    playerData._CibleCamera = obj;
+                }*/
+                    
+
                 if (inverseTrigger.activeSelf)
                 { 
                     target = Quaternion.Euler(0, valRotaDeg, 0);
                     inverseTrigger2.SetActive(true);
+                    colliderCam2.SetActive(true);
                     inverseTrigger.SetActive(false);
+                    colliderCam.SetActive(false);
                     
                 }
                 else
                 {
                     target = Quaternion.Euler(0, baseRotaDeg, 0);
                     inverseTrigger.SetActive(true);
+                    colliderCam.SetActive(true);
                     inverseTrigger2.SetActive(false);
+                    colliderCam2.SetActive(false);
                     
                 }
                 
                 obj.transform.position = new Vector3(transform.position.x, obj.transform.position.y, transform.position.z);
+
+                
             }
             obj.transform.rotation = Quaternion.Slerp(obj.transform.rotation, target, Time.deltaTime * speedRotation);
-            
+
+            /*if(obj.transform.rotation == target && hasColliderCam)
+                colliderCam.SetActive(true); */
+
         }
         
     }
