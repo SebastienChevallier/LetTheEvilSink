b74be07a5f436f5508b58fd0ab07ac1645d97a17
Boeingman <nicolas.restouin@gmail.com>
2023-01-23 10:01:54 +0100

    Script clear

--- Assets/Scripts/SO/SO_Player.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Player", menuName = "Scriptable Objects/Player", order = 0)]
-public class SO_Player : ScriptableObject
-{
-    public string username;
-    public Sprite portrait;
-    public int mana;
-}
