aaa7fa963760bd2560586fc724e53654ebb4b824
Sebastien chevallier <44536776+The-Real-Seb@users.noreply.github.com>
2023-02-21 13:33:23 +0100

    Build

--- Assets/Scripts/Mini-jeux/Carte.cs
+++ Assets/Scripts/Mini-jeux/Carte.cs
@@ -86,9 +86,7 @@ public class Carte : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHan
             if(timer > minTime && timer < maxTime)
             {
                 completed = true;
-                Debug.Log("Complete");
-                SceneManager.UnloadSceneAsync("Carte");
-                SceneManager.LoadScene("Mini jeux", LoadSceneMode.Additive);
+                
                 CancelDrag();
             }
             else if (timer < minTime)
