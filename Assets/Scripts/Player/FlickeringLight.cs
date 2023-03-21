8efdfd9a05b40e58c3c33165ff6c76219d6d4eae
Sebastien chevallier <44536776+The-Real-Seb@users.noreply.github.com>
2023-03-21 10:42:18 +0100

    event

---
+++ Assets/Scripts/Player/FlickeringLight.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlickeringLight : MonoBehaviour
+{
+    public float flickPower;
+    public float delayFlick;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
