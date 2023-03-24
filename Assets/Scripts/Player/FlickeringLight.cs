d08e45d8867e54e4b461a19d0296c9f9753d6e15
Sebastien chevallier <44536776+The-Real-Seb@users.noreply.github.com>
2023-03-24 15:51:31 +0100

    ImportRigAndCreature

--- Assets/Scripts/Player/FlickeringLight.cs
+++ Assets/Scripts/Player/FlickeringLight.cs
@@ -6,16 +6,30 @@ public class FlickeringLight : MonoBehaviour
 {
     public float flickPower;
     public float delayFlick;
+    private float delay;
     
-    // Start is called before the first frame update
-    void Start()
+    private Light _light;
+    private float _initialIntensity;
+    private float _initialRange;
+    
+    private void Start()
     {
-        
+        _light = GetComponent<Light>();
+        _initialIntensity = _light.intensity;
+        _initialRange = _light.range;
+        delay = Time.time + delayFlick;
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    private void Update()
     {
-        
+        if(Time.time > delay)
+            Flickering();
+    }
+    
+    private void Flickering()
+    {
+        _light.intensity = Random.Range(_initialIntensity - flickPower, _initialIntensity + flickPower);
+        _light.range = Random.Range(_initialRange - flickPower, _initialRange + flickPower);
+        delay = Time.time + delayFlick;
     }
 }
