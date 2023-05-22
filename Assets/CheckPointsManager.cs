28672fd439729ab5d6090745f8e2edeaad04c261
Sebastien chevallier <44536776+The-Real-Seb@users.noreply.github.com>
2023-05-22 15:19:08 +0200

    Import UI Minijeu

---
+++ Assets/CheckPointsManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using BaseTemplate.Behaviours;
+using UnityEngine;
+
+public class CheckPointsManager : MonoSingleton<CheckPointsManager>
+{
+    public GameObject lastCheckPoint;
+}
