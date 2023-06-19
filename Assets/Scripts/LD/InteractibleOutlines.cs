b45199c15850fbeda738ca954f1e5896cbe23dc8
SimplyMarket <louis.bich@gmail.com>
2023-06-19 15:26:19 +0200

    Outline script

---
+++ Assets/Scripts/LD/InteractibleOutlines.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class InteractibleOutlines : MonoBehaviour
+{
+    public GameObject outline;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) outline.SetActive(true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player")) outline.SetActive(false);
+    }
+}
