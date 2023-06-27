96efc5860562d5e49355f09203e84a80308e18da
SimplyMarket <louis.bich@gmail.com>
2023-06-27 23:43:42 +0200

    debug

--- Assets/Scripts/MenuPause.cs
+++ Assets/Scripts/MenuPause.cs
@@ -3,12 +3,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class MenuPause : MonoBehaviour
 {
     public GameObject panel;
     public GameObject panelObjectif;
     public So_Player _Player;
+
+    public VideoPlayer _VideoPlayer;
     
     private void Update()
     {
@@ -16,15 +19,18 @@ public class MenuPause : MonoBehaviour
         {
             if (panel.activeSelf)
             {
+                if (_VideoPlayer.isPaused) _VideoPlayer.Play();
+
                 Resume();
             }
             else
             {
+                if (_VideoPlayer.isPlaying) _VideoPlayer.Pause();
+
                 panel.SetActive(true);
                 panelObjectif.SetActive(false);
                 if (Player_Movements.Instance) Player_Movements.Instance.planeAnimator.SetFloat("Speed", 0);
                 _Player._CanMove = false;
-                Debug.Log(_Player.inMenu);
                 _Player.inMenu = true;
             }
         }
--- Assets/TriggerCinematique.cs
+++ Assets/TriggerCinematique.cs
@@ -8,10 +8,16 @@ public class TriggerCinematique : MonoBehaviour
     public GameObject cinematique;
     private bool cinematiquePlayed = false;
 
+    public Trigger_Minijeu triggerMinijeu;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player") && !cinematiquePlayed)
         {
+            if (triggerMinijeu)
+            {
+                if (!triggerMinijeu.validated) return;
+            }
             cinematique.SetActive(true);
             cinematiquePlayed = true;
         }
